88b6f4ee42e949fe5fe99b2cd1df544b4c628eca
Teresa Marques <taotaodoce@gmail.com>
2022-07-28 16:37:14 -0300

    requisito 4 concluído

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,19 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some((elemento) => elemento.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const funcionarios = data.employees.filter((manager) =>
+    manager.managers.includes(managerId));
+  const listaFuncionarios = funcionarios.map(
+    (elemento) => `${elemento.firstName} ${elemento.lastName}`,
+  );
+  return listaFuncionarios;
 }
 
 module.exports = { isManager, getRelatedEmployees };
