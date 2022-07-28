dc4ba24aded30af736ab0ea2e2c1ec855cf77a73
Teresa Marques <taotaodoce@gmail.com>
2022-07-28 15:41:30 -0300

    requisito 2 concluído

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalData = data.species.find((resident) => resident.name === animal);
+  const idadeMinima = animalData.residents.every(
+    (residentAnimal) => residentAnimal.age >= age,
+  );
+  return idadeMinima;
 }
 
 module.exports = getAnimalsOlderThan;
