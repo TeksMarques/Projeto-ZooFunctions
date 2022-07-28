3d0bf3e002ec3b8a1d42b30c8c5e33cdcadcaa41
Teresa Marques <taotaodoce@gmail.com>
2022-07-28 15:53:54 -0300

    requisito 3 concluído

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (typeof employeeName === 'undefined') {
+    return {};
+  }
+  return data.employees.find((elemento) =>
+    [elemento.firstName, elemento.lastName].includes(employeeName));
 }
 
 module.exports = getEmployeeByName;
