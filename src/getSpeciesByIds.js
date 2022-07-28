351a899f30c5478bc21dfb48d0a20a21b3030129
Teresa Marques <taotaodoce@gmail.com>
2022-07-28 15:03:52 -0300

    requisito 1 concluído

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  //inicio do projeto
+  return data.species.filter((especies) =>
+    ids.find((id) => id === especies.id));
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
