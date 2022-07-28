adb5fda8094d9b2f29b719d403683763258e9f45
Teresa Marques <taotaodoce@gmail.com>
2022-07-28 18:03:57 -0300

    requisitos 6, 7 e 15

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,18 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Teste não passando argumentos. Deverá retornar o objeto:', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+  it('Para os argumentos Monday e 09:00-AM deve retornar a string "The zoo is closed"', () => {
+
+  });
+});
