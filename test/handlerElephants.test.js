c459bb55fcc2ffdd804b1e371bfea966dc7ece2a
Teresa Marques <taotaodoce@gmail.com>
2022-07-28 17:54:46 -0300

    requisitos 6, 7 e 15 concluídos

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,31 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Total de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Para o argumento name', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Para a média da idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('Para a sua localização dos elefantes dentro do zoologico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Para o numero de popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  });
+  it('Para os dias de visita aos elefantes', () => {
+    expect(handlerElephants('availability')).not.toContain('Monday');
+  });
+  it('Para quando não for passado parametro', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Para quando for passado um objeto vazio', () => {
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Passada uma string que não contempla uma funcionalidade deve retornar null', () => {
+    expect(handlerElephants('teresa')).toBeNull();
+  });
+});
