a5d40a5d01e6c4d4666f13fd76abebb3c56a6b01
Teresa Marques <taotaodoce@gmail.com>
2022-07-29 15:28:33 -0300

    requisito 8 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,31 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const pessoas = { child: 0, adult: 0, senior: 0 };
+  entrants.filter((elemento) => {
+    if (elemento.age < 18) {
+      pessoas.child += 1;
+    } else if (elemento.age >= 18 && elemento.age < 50) {
+      pessoas.adult += 1;
+    } else {
+      pessoas.senior += 1;
+    }
+    return pessoas;
+  });
+  return pessoas;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const count = countEntrants(entrants);
+  let calculo = 0;
+  calculo += count.child * data.prices.child;
+  calculo += count.adult * data.prices.adult;
+  calculo += count.senior * data.prices.senior;
+  return calculo;
 }
 
 module.exports = { calculateEntry, countEntrants };
