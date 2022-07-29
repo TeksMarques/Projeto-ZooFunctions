d8ffe8598116b9276ecd24248d34f9aefbe7ab4e
Teresa Marques <taotaodoce@gmail.com>
2022-07-29 19:48:13 -0300

    tentado resolver a 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+const id = 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'
+
+const funcionario = (id) => data.employees.find((employe) => employe.id === id)
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  
 }
-
+console.log(getOldestFromFirstSpecies(id))
 module.exports = getOldestFromFirstSpecies;
