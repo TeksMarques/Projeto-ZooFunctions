3241e7618388e1341e3eb80ce0b336aca156899c
Teresa Marques <taotaodoce@gmail.com>
2022-07-29 13:31:19 -0300

    requisito 5 concluído

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const animais = {};
+  if (typeof animal === 'undefined') {
+    data.species.forEach((element) => {
+      animais[element.name] = element.residents.length;
+    });
+    return animais;
+  }
+  const { specie: bicho, sex = 'genero' } = animal;
+  const especie = data.species.find((specie) => specie.name === bicho);
+  if (sex === 'genero') {
+    return especie.residents.length;
+  }
+  return especie.residents.filter((unico) => unico.sex === sex).length;
 }
 
 module.exports = countAnimals;
