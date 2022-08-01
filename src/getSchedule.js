2d2a0041b796a832fed17c29a6ce0e8d7346faa0
Teresa Marques <taotaodoce@gmail.com>
2022-08-01 14:07:11 -0300

    tentando finalizar o requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,61 @@
 const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function getAnimais (dia) {
+  //console.log(species)
+  let animals=[]
+  species.forEach(s => {
+    if(s.availability.includes(dia)){
+      animals.push(s.name)
+    }    
+  })
+  return animals;
 }
 
+function diasAbertos(dia) {
+  const dias = Object.keys(hours);
+  if(dias.includes(dia)){
+    if(dia === 'Monday'){
+      return {
+        officeHour: 'CLOSED',
+        exhibition: ''
+      }
+    }
+    return {
+      officeHour: `Open from ${hours[dia].open}am until ${hours[dia].close}pm`,
+      exhibition: getAnimais(dia)
+    }
+  } else {
+    return undefined;
+  }
+}
+
+
+function getDiasByAnimal (animal) {
+  let dias = ''
+    species.forEach(s => {
+      if(s.name.includes(animal)){
+        dias = s.availability
+      } else {
+        return undefined
+      }
+    })
+  return dias 
+}
+
+
+function verificacao (schedule) {
+  if (Object.keys(hours).includes(schedule)){
+    diasAbertos(schedule)
+  } else {
+    getDiasByAnimal(schedule)
+  }
+}
+
+function getSchedule(scheduleTarget) {
+  // seu código aqui  
+    verificacao(scheduleTarget)
+  }
+
+ console.log(getSchedule('lions')) 
 module.exports = getSchedule;
