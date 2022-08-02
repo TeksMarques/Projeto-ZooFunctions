1984e279ccce278250d6f6c55b201a5a70a5a88b
Teresa Marques <taotaodoce@gmail.com>
2022-08-02 12:37:44 -0300

    requisito 11 concluído

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getValues(nome) {
+  const animais = nome.map((element) => species.find((animal) => element === animal.id));
+  const nomesAnimais = animais.map((nomes) => nomes.name);
+  const local = animais.map((nome3) => nome3.location);
+  return [nomesAnimais, local];
+}
+
+function final(valor) {
+  const array = getValues(valor.responsibleFor);
+  let resultado = {};
+  resultado = {
+    id: valor.id,
+    fullName: `${valor.firstName} ${valor.lastName}`,
+    species: array[0],
+    locations: array[1],
+  };
+  return resultado;
+}
+
+function getEmployeesCoverage(valor) {
+  if (!valor) {
+    const arrayTotal = [];
+    employees.forEach((empregado) => {
+      const finalizado = final(empregado);
+      arrayTotal.push(finalizado);
+    });
+    return arrayTotal;
+  }
+  const entry = Object.values(valor);
+  const entrada = entry.toString();
+  const nome = employees.find((nome4) => nome4.firstName === entrada);
+  const sobreNome = employees.find((nome1) => nome1.lastName === entrada);
+  const id = employees.find((nome2) => nome2.id === entrada);
+  if (nome) { return final(nome); }
+  if (sobreNome) { return final(sobreNome); }
+  if (id) { return final(id); }
+
+  throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,61 +1,57 @@
 const data = require('../data/zoo_data');
 const { species, hours } = require('../data/zoo_data');
 
-function getAnimais (dia) {
-  //console.log(species)
-  let animals=[]
-  species.forEach(s => {
-    if(s.availability.includes(dia)){
-      animals.push(s.name)
-    }    
-  })
+function getAnimais(dia) {
+  // console.log(species)
+  const animals = [];
+  species.forEach((s) => {
+    if (s.availability.includes(dia)) {
+      animals.push(s.name);
+    }
+  });
   return animals;
 }
 
 function diasAbertos(dia) {
   const dias = Object.keys(hours);
-  if(dias.includes(dia)){
-    if(dia === 'Monday'){
+  if (dias.includes(dia)) {
+    if (dia === 'Monday') {
       return {
         officeHour: 'CLOSED',
-        exhibition: ''
-      }
+        exhibition: '',
+      };
     }
     return {
       officeHour: `Open from ${hours[dia].open}am until ${hours[dia].close}pm`,
-      exhibition: getAnimais(dia)
-    }
-  } else {
-    return undefined;
+      exhibition: getAnimais(dia),
+    };
   }
+  return undefined;
 }
 
-
-function getDiasByAnimal (animal) {
-  let dias = ''
-    species.forEach(s => {
-      if(s.name.includes(animal)){
-        dias = s.availability
-      } else {
-        return undefined
-      }
-    })
-  return dias 
+function getDiasByAnimal(animal) {
+  let dias = '';
+  species.forEach((s) => {
+    if (s.name.includes(animal)) {
+      dias = s.availability;
+    } else {
+      return undefined;
+    }
+  });
+  return dias;
 }
 
-
-function verificacao (schedule) {
-  if (Object.keys(hours).includes(schedule)){
-    diasAbertos(schedule)
+function verificacao(schedule) {
+  if (Object.keys(hours).includes(schedule)) {
+    diasAbertos(schedule);
   } else {
-    getDiasByAnimal(schedule)
+    getDiasByAnimal(schedule);
   }
 }
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui  
-    verificacao(scheduleTarget)
-  }
+  // seu código aqui
+  verificacao(scheduleTarget);
+}
 
- console.log(getSchedule('lions')) 
 module.exports = getSchedule;
